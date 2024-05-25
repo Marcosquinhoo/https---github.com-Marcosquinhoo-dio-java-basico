b5edaad056021b233ce3313865dd80f5968e5cdb
Marcos <quinhomarcosjr@gmail.com>
2024-05-25 02:56:12 -0300

    meu projeto banco

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,7 @@
 public class ContaTerminal {
     public static void main(String[] args) {
-        
-        System.out.println("Olá conta banco");
+
+        System.out.println("Olá começar projeto banco");
     }
     
 }
