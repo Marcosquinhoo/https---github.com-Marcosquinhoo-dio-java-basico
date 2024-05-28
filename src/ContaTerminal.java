b9245b58b0581158ac450b3be06cde3f83cf8f99
Marcos <quinhomarcosjr@gmail.com>
2024-05-27 21:41:45 -0300

    Auterando dados

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,7 @@
 public class ContaTerminal {
     public static void main(String[] args) {
 
-        System.out.println("Olá começar projeto banco");
+        System.out.println("Olá   testando projeto no GitHub");
     }
-    
+
 }
